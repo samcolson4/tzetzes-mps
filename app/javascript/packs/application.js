cb050120197dc645204315a7de08ccfd5b2cc942
samcolson4 <samcolson4@gmail.com>
2021-06-15 13:33:35 +0100

    Add navbar styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,15 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  const navbarHeight = 100;
+
+  const distanceFromTop = Math.abs(
+    document.body.getBoundingClientRect().top
+  );
+
+  if (distanceFromTop >= navbarHeight) navbar.classList.add("fixed-top");
+  else navbar.classList.remove("fixed-top");
+});
