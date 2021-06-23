1c8526c80be1f6a1ce333716e6e21a1f1ef6bd58
samcolson4 <samcolson4@gmail.com>
2021-06-23 17:32:20 +0100

    Add positive search test

--- spec/features/articles_views_spec.rb
+++ spec/features/articles_views_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
 
 feature "User can view all articles" do
-  scenario "User can view all articles" do
+  scenario "User can view a list of all articles" do
     create_article
     sign_up
     expect(page).to have_content("Articles")
     click_link("Articles")
     expect(page).to have_content("All articles")
     expect(page).to have_content("MP saves dog")
+  end
+
+  scenario "User can search for a specific article" do
+     create_article
+     sign_up
+     find(:css, "input[id$='search']").set("Dog")
+     click_button "Search"
+     expect(page).to have_current_path("/search?search=Dog")
+     expect(page).to have_content("MP saves dog")
   end
 
   # scenario "User can view MPs on further pages of the site" do
