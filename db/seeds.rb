d737cb271d81aa32f43c50179ddec1cf3825084a
samcolson4 <samcolson4@gmail.com>
2021-06-23 17:26:28 +0100

    Update article seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ articles = Article.create([
   "url":"https://www.melstridemp.com", 
   "tag": "Local News", 
   "article_text": "Local MP, Mel Stride, today visited a local post office", 
-  "headline": "MP Visits Post Office"
+  "headline": "MP Visits Post Office"  
   }
 ])
 
