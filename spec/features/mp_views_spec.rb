7068f93f28ab6413971db47c59386376cede1a82
samcolson4 <samcolson4@gmail.com>
2021-06-23 17:51:05 +0100

    Add test for viewing MPs' profiles

--- spec/features/mp_views_spec.rb
+++ spec/features/mp_views_spec.rb
@@ -15,4 +15,13 @@ feature "User can view all MPs" do
     expect(page).to have_content("Robert Courts MP")
   end
 
+  scenario "User can view MPs' profiles" do
+    sign_up
+    click_link("MPs")
+    click_link("Ms Diane Abbott MP")
+    expect(page).to have_current_path("/mps/hackney-north-and-stoke-newington")
+    expect(page).to have_content("Party: Labour")
+    expect(page).to have_content("First elected: 1987")
+  end
+
 end
