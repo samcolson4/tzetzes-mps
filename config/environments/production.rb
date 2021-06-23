2b733f243187c50b025a9ebdafa4039c2a845660
samcolson4 <samcolson4@gmail.com>
2021-06-23 15:34:52 +0100

    Add bundler script 9-16

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,20 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  namespace :deploy do
+    desc 'Config bundler'
+  
+    task :config_bundler do
+      on roles(/.*/) do
+        execute :bundle, 'config', '--local deployment', true
+        execute :bundle, 'config', '--local', 'without', "development:test"
+        execute :bundle , 'config', '--local', 'path', shared_path.join('bundle')
+      end
+    end
+  end
+  
+  before 'bundler:install', 'deploy:config_bundler'
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
