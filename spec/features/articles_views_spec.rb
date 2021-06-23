98ffacf28b8c5e71a0cc81e7c53a45d7e34da46d
samcolson4 <samcolson4@gmail.com>
2021-06-23 17:26:43 +0100

    Add article tests

--- spec/features/articles_views_spec.rb
+++ spec/features/articles_views_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 feature "User can view all articles" do
   scenario "User can view all articles" do
+    create_article
     sign_up
     expect(page).to have_content("Articles")
     click_link("Articles")
     expect(page).to have_content("All articles")
+    expect(page).to have_content("MP saves dog")
   end
 
   # scenario "User can view MPs on further pages of the site" do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require_relative './helpers/authentication'
+require_relative './helpers/articles'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
