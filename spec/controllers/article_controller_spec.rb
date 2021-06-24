d6cad0be0825051eabf699cb60768877c5d4ea71
samcolson4 <samcolson4@gmail.com>
2021-06-24 09:13:07 +0100

    Refactor

--- spec/controllers/article_controller_spec.rb
+++ spec/controllers/article_controller_spec.rb
@@ -4,8 +4,6 @@ describe ArticlesController do
   describe "POST mps/:id/articles | #create" do
     it "adds an article to the database" do
       Mp.create(id: 999, name: "Bob", mp_serial: "999")
-      # mp = Mp.find_by(id: 999)
-      # allow(Mp).to receive(:find_by).and_return(mp)
 
       Article.create(headline: "MP saves dog", datetime:"10:54", url:"www.melstridemp.com", tag:"Local News", article_text:"Ipsum Lorem mp dog saver!", mp_id:'999')
 
