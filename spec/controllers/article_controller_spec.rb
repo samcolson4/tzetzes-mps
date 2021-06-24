4cf7cbc7113a207d1c3a8e02a45e0838b782e275
samcolson4 <samcolson4@gmail.com>
2021-06-24 09:12:49 +0100

    Add test for failing article creation

--- spec/controllers/article_controller_spec.rb
+++ spec/controllers/article_controller_spec.rb
@@ -4,17 +4,25 @@ describe ArticlesController do
   describe "POST mps/:id/articles | #create" do
     it "adds an article to the database" do
       Mp.create(id: 999, name: "Bob", mp_serial: "999")
-      mp = Mp.find_by(id: 999)
-      allow(Mp).to receive(:find_by).and_return(mp)
+      # mp = Mp.find_by(id: 999)
+      # allow(Mp).to receive(:find_by).and_return(mp)
 
       Article.create(headline: "MP saves dog", datetime:"10:54", url:"www.melstridemp.com", tag:"Local News", article_text:"Ipsum Lorem mp dog saver!", mp_id:'999')
 
       expect(Article.find_by(headline: "MP saves dog")).to be
     end
+
+    it "does not add an article, if it has the wrong serial" do
+      Mp.create(id: 999, name: "Bob", mp_serial: "999")
+
+      post :create, params: { headline: "MP saves dog", datetime:"10:54", url:"www.melstridemp.com", tag:"Local News", article_text:"Ipsum Lorem mp dog saver!", mp_id:'999', mp_serial:'0' }
+
+      expect(Article.find_by(headline: "MP saves dog")).not_to be
+    end
   end
 
   describe "#search" do
-    it "Redirects to '/' if blank" do
+    it "Redirects to '/about' if blank" do
       post :search, params: { search: "" }
       expect(response).to redirect_to("/about")      
     end
