cd2831d16e86d6d5d458e4ca41165767c35069ea
samcolson4 <samcolson4@gmail.com>
2021-06-29 14:17:55 +0100

    Add @tzetzes secret keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,13 +68,16 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   #MAILER
-  # config.action_mailer.default_url_options = {:host => 'tzetzes.com'}
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   :address => "127.0.0.1",
-  #   :port    => 25,
-  #   :domain  => 'tzetzes.com'
-  # }
+  config.action_mailer.default_url_options = {:host => 'tzetzes.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :user_name            => ENV['GMAIL_USER_SECRET'],
+    :password             => ENV['GMAIL_PW_SECRET'],
+    :authentication       => "plain",
+    :port    => 587,
+    :domain  => 'tzetzes.com'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
