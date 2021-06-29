adabffa1c8b193b28a6e82736b7d9380d1b167df
samcolson4 <samcolson4@gmail.com>
2021-06-29 09:09:48 +0100

    Add mailcatcher development server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,11 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Mailer
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
