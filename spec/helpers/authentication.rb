8d615a2c4045b3988a6ecdb7d6b2a8a11c43e1b9
samcolson4 <samcolson4@gmail.com>
2021-07-01 07:41:06 +0100

    Update helper methods to account for devise

--- spec/helpers/authentication.rb
+++ spec/helpers/authentication.rb
@@ -5,7 +5,27 @@ def sign_up
   fill_in "Surname", with: "Bobby"
   fill_in "Email", with: "bob@test.com"
   fill_in "Password", with: "Bobobobob"
+  fill_in "Password confirmation", with: "Bobobobob"
   click_button "Sign up"
+  User.find_by(email: "bob@test.com").confirm
+  visit "/"
+end
+
+def sign_up_sign_in
+  visit "/"
+  click_link "Register"
+  fill_in "user_name", with: "Bob"
+  fill_in "Surname", with: "Bobby"
+  fill_in "Email", with: "bob@test.com"
+  fill_in "Password", with: "Bobobobob"
+  fill_in "Password confirmation", with: "Bobobobob"
+  click_button "Sign up"
+  User.find_by(email: "bob@test.com").confirm
+  visit "/"
+  click_link "Sign in"
+  fill_in "Email", with: "bob@test.com"
+  fill_in "Password", with: "Bobobobob"
+  click_button "Log in"
 end
 
 def sign_out 
