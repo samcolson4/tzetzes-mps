51654c19d8965608fe990c41adbb94bb7cbaf3bc
samcolson4 <samcolson4@gmail.com>
2021-07-01 09:46:40 +0100

    Remove jquery mention

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,15 +11,3 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-// document.addEventListener("scroll", function () {
-//   const navbar = document.querySelector(".navbar");
-//   const navbarHeight = 100;
-
-//   const distanceFromTop = Math.abs(
-//     document.body.getBoundingClientRect().top
-//   );
-
-//   if (distanceFromTop >= navbarHeight + 100) navbar.classList.add("fixed-top");
-//   else navbar.classList.remove("fixed-top");
-// });
