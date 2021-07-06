2a167528b2f948caa9d657c5b49d30af4cae3f02
samcolson4 <samcolson4@gmail.com>
2021-07-06 10:20:28 +0100

    Update method to fix test

--- spec/features/user_can_register_spec.rb
+++ spec/features/user_can_register_spec.rb
@@ -27,7 +27,7 @@ feature "Signup" do
   scenario "it prevents singup with duplicated email address" do
     sign_up_sign_in
     click_link "Sign out"
-    sign_up
+    sign_up_basic
     expect(page).to have_content("Email has already been taken")
   end
 
