675e77f85ced3f4ba27b91e7d651e0815bdfc46b
samcolson4 <samcolson4@gmail.com>
2021-07-20 22:15:36 +0100

    Fix API tests

--- spec/features/api_spec.rb
+++ spec/features/api_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 feature "User can view the api explanation page" do
   scenario "User can view the privacy policy page" do
     visit "/api"
-    expect(page).to have_content("$ curl tzetzes.com")
+    expect(page).to have_content("$ curl http://www.tzetzes.com")
   end
 
   scenario "User can view the API page, after clicking the link in the header" do
     sign_up_sign_in
     visit "/about"
     click_link("API")
-    expect(page).to have_content("$ curl tzetzes.com")
+    expect(page).to have_content("$ curl http://www.tzetzes.com")
   end
 end
